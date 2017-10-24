f5d8c5f19e290f7a03e659cbd32db39b09da8b03
ttran9 <toddtran9@gmail.com>
2017-10-24 16:18:50 -0700

    Video 245. Removing code that works with Spring MVC but not Spring WebFlux.

--- src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -74,34 +74,34 @@ public class ImageControllerTest {
     }
 
 
-    @Test
-    public void renderImageFromDB() throws Exception {
-
-        //given
-        RecipeCommand command = new RecipeCommand();
-        command.setId("1");
-
-        String s = "fake image text";
-        Byte[] bytesBoxed = new Byte[s.getBytes().length];
-
-        int i = 0;
-
-        for (byte primByte : s.getBytes()){
-            bytesBoxed[i++] = primByte;
-        }
-
-        command.setImage(bytesBoxed);
-
-        when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
-
-        //when
-        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
-
-        byte[] reponseBytes = response.getContentAsByteArray();
-
-        assertEquals(s.getBytes().length, reponseBytes.length);
-    }
+//    @Test
+//    public void renderImageFromDB() throws Exception {
+//
+//        //given
+//        RecipeCommand command = new RecipeCommand();
+//        command.setId("1");
+//
+//        String s = "fake image text";
+//        Byte[] bytesBoxed = new Byte[s.getBytes().length];
+//
+//        int i = 0;
+//
+//        for (byte primByte : s.getBytes()){
+//            bytesBoxed[i++] = primByte;
+//        }
+//
+//        command.setImage(bytesBoxed);
+//
+//        when(recipeService.findCommandById(anyString())).thenReturn(Mono.just(command));
+//
+//        //when
+//        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
+//                .andExpect(status().isOk())
+//                .andReturn().getResponse();
+//
+//        byte[] reponseBytes = response.getContentAsByteArray();
+//
+//        assertEquals(s.getBytes().length, reponseBytes.length);
+//    }
 
 }
